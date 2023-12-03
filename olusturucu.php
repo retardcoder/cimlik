bda1d1f41b8c9c959e93e90ec91a71810c3434bb
lunartheboss <150518564+lunartheboss@users.noreply.github.com>
2023-12-03 21:37:14 +0300

    177
    1qjj

---
+++ olusturucu.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Türkiye Kimlik Oluşturma ROXY REİS</title>
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="img/favicon.png" type="image/png">
+<script type="text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "js/tag.js", "ym");
+</script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow py-3">
+<div class="container">
+<a class="navbar-brand mx-auto" href="https://breached.vc/">Türkiye Kimlik Oluşturma ROXY REİS</a>
+</div>
+</nav>
+<div class="container">
+<div class="card text-white bg-primary rounded shadow mt-4">
+<div class="card-body">
+<form action="" class="row" id="form">
+<div class="col-lg-6">
+<div>İsim:</div>
+<input class="form-control d-block mt-2 shadow" name="name" placeholder="Enter the name to be written on the ID." required>
+</div>
+<div class="col-lg-6 mt-3 mt-lg-0">
+<div>Soy Adı:</div>
+<input class="form-control d-block mt-2 shadow" name="surname" placeholder="Enter the last name to be written on the ID." required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Doğum tarihi:</div>
+<input class="form-control d-block mt-2 shadow" name="birth_date" type="date" required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Cinsiyet:</div>
+<select name="gender" class="form-control d-block mt-2 shadow">
+<option value="E / M" option>Erkek</option>
+<option value="K / F">Kadın</option>
+</select>
+</div>
+<div class="col-lg-6 mt-3">
+<div>T.C. Numarası:</div>
+<input class="form-control d-block mt-2 shadow" name="tckn" placeholder="Enter the T.C. ID number that will be written on the ID. (Ex: 52831396760)" required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Seri Numarası:</div>
+<input class="form-control d-block mt-2 shadow" name="document_number" placeholder="Enter the serial number to be written on the ID. (Ex: A12L34567)" required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Son kullanma tarihi:</div>
+<input class="form-control d-block mt-2 shadow" name="valid_until" type="date" required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Milliyet:</div>
+<input class="form-control d-block mt-2 shadow" value="T.C./TUR" readonly>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Anne Adı:</div>
+<input class="form-control d-block mt-2 shadow" name="mother_name" placeholder="Enter the mother's name to be written on the ID." required>
+</div>
+<div class="col-lg-6 mt-3">
+<div>Baba Adı:</div>
+<input class="form-control d-block mt-2 shadow" name="father_name" placeholder="Enter the father's name to be written on the ID." required>
+</div>
+<div class="col-lg-12 mt-3">
+<div>Kimlik Fotosu:</div>
+<input class="form-control d-block mt-2 shadow" type="file" name="image" accept="image/*" required>
+</div>
+<div class="col-lg-12 mt-4 d-flex">
+<div class="flex-grow-1">
+<button type="submit" class="btn btn-primary shadow">Create Identity</button>
+</div>
+</div>
+</form>
+</div>
+</div>
+<div class="card text-white bg-primary rounded shadow my-4">
+<div class="card-body">
+<div class="row">
+<div class="h4 mb-2">Created ID Images</div>
+<div class="text-one">UYARI:Bu site tamamen eğitim amaçlıdır eğer burda yaptıklarınızı kötü birşey için kullanırsanız bundan https://hile-diyari.rf.gd/ sorumlu değildir.
+</div>
+<div class="text-two d-none">UYARI:Bu site tamamen eğitim amaçlıdır eğer burda yaptıklarınızı kötü birşey için kullanırsanız bundan https://hile-diyari.rf.gd/ sorumlu değildir.</div>
+<div class="col-lg-6 mt-3">
+<img src="img/front-empty.png" class="front-image mw-100">
+<button class="btn btn-primary shadow mt-3" id="download-front" disabled>Download Image</button>
+</div>
+<div class="col-lg-6 mt-3">
+<img src="img/back-empty.png" class="back-image mw-100">
+<button class="btn btn-primary shadow mt-3" id="download-back" disabled>Download Image</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="side-container">
+<div class="front">
+<img src="" class="face">
+<img src="" class="face-right">
+<div class="tckn"></div>
+<div class="name"></div>
+<div class="surname"></div>
+<div class="birth_date"></div>
+<div class="gender"></div>
+<div class="document_number"></div>
+<div class="valid_until"></div>
+</div>
+<div class="back">
+<div class="mother_name"></div>
+<div class="father_name"></div>
+<div class="mrz"></div>
+</div>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/domtoimage.min.js"></script>
+<script src="js/script.min.js"></script>
+</body>
+</html>
